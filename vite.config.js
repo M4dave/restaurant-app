eed7e6a0eeef0406d01cfe810a82a78cb97c5b0d
M4dave <davidjohncruz@gmail.com>
2024-09-06 12:42:55 -0400

    updated backend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173, // Ensure this matches the port expected by Render
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 5173,       // Ensure this matches the port Render expects
   },
-});
+})
